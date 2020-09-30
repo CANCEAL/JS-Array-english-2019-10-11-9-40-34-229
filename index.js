5538572ac71909362c508b26190bb36ec39963b4
ALFRED CANCEJO (GEDI-ISD-OOCLL/MNL) <CANCEAL@OOCL.COM>
2020-09-30 18:36:42 +0800

    Completed all tasks in index.js

--- index.js
+++ index.js
@@ -2,25 +2,54 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+if (typeof(a) == 'object' && typeof(b) == 'object'){
+    console.log("Variables 'a' and 'b' is an Array.");
+} else if (typeof(a) == 'string' && typeof(b) == 'object'){
+    console.log("Variable 'a' is a String and 'b' is an Array.");
+} else if (typeof(a) == 'object' && typeof(b) == 'string'){
+    console.log("Variable 'a' is an Array and 'b' is a String.");
+} else {
+    console.log("Unable to be defined.");
+}
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+    for(i = 0; i < 5; i++) {
+        a[i] = a[i] * 2;
+    }
+console.log(a);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+console.log(colors.join(" "));
+console.log(colors.join("+"));
+console.log(colors.join())
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a.sort(function(i,j) {
+    return j-i;
+})
+console.log(a);
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var maxFrequency = 1, ctr = 0, maxValue;
+    for (var i=0; i<a.length; i++) {
+        for (var j=i; j<a.length; j++) {
+            if (a[i] == a[j]) {
+                ctr = ctr + 1;
+                if (maxFrequency<ctr) {
+                    maxFrequency=ctr;
+                    maxValue = a[i];
+                }
+            }
+        } ctr=0;
+    }
+console.log(maxValue)
