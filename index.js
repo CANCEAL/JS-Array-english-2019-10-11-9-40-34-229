ee9ec1efc4401c89ae71081a20d68d651088ce99
ALFRED CANCEJO (GEDI-ISD-OOCLL/MNL) <CANCEAL@OOCL.COM>
2020-10-01 08:12:47 +0800

    Updated codes in index.js

--- index.js
+++ index.js
@@ -32,10 +32,16 @@ console.log(colors.join())
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-a.sort(function(i,j) {
-    return j-i;
-})
-console.log(a);
+for (var i=0; i<a.length; i++) {
+    for (var j=i; j<a.length; j++) {
+        if (a[i] < a[j]) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            }
+        } 
+    }
+console.log(a)
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
